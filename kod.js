fec55b86c890e2053910901ad1f0346d13bcce52
SujanthanR <Sujanthan.r@live.com>
2017-11-17 13:22:27 +0100

    jQuery
    jQuery things

--- kod.js
+++ kod.js
@@ -2,4 +2,30 @@ $(document).ready(function() {
     $("#openinfo").click(function(){
         $("#info").toggle();
     });
-});
+
+    $("li").dblclick(function(){
+        $(this).toggleClass("selected");
+
+    });
+
+    $("#bgcolor").change(function(){
+        $("body").css("background-color", $(this).val());
+    });
+    
+    $("#textcolor").change(function(){ 
+        $("body").css("color", $(this).val());
+    
+    });
+    
+    $("textarea").focusout(function(){
+        $("h1").text( $(this).val());
+    });
+    
+    $("textarea").focusin(function(){
+        $("textarea").text("", $(this).val());
+    });
+}); 
+   
+    
+                     
+
